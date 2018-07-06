5afb7e417ec63d559f3931bdb05e6bc339fb8f71
Joseph Diragi <joyod3@gmail.com>
2018-07-06 15:20:02 -0400

    Adds initial logic for talking with android side

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(new MyApp());
 
@@ -25,7 +28,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  static const platform = const MethodChannel('com.diragi.1337/shell');
+  bool enabled = false;
+
+  Future<Null> _runShelScript() async {
+    bool status;
+    try {
+      status = await platform.invokeMethod('runShellScript');
+    } on PlatformException catch (e) {
+      status = false;
+    }
+
+    setState(() {
+      enabled = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
