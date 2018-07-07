3fb4e2bed496461245afc22d6299d8529265dcde
Joseph Diragi <joyod3@gmail.com>
2018-07-07 14:14:34 -0400

    Cleans up android code

--- android/app/src/main/kotlin/com/example/m2messaging/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/m2messaging/MainActivity.kt
@@ -1,72 +1,34 @@
 package com.example.m2messaging
 
 import android.os.Bundle
-import android.os.AsyncTask
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugin.common.MethodChannel
 import com.jaredrummler.android.shell.Shell
-import com.jaredrummler.android.shell.CommandResult
+import io.flutter.plugins.GeneratedPluginRegistrant
+import kotlinx.coroutines.experimental.*
 
-class MainActivity(): FlutterActivity() {
+class MainActivity : FlutterActivity() {
   private val CHANNEL = "com.diragi.1337/shell"
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    MethodChannel(flutterView, CHANNEL).setMethodCallHandler{ call, result ->
-      if (call.method == "runShellScript") {
-        val shellScript = checkForRoot()
-        if(shellScript) {
-          result.success(true);
-        } else {
-          result.error("UNAVAILABLE", "Something horrible happened", null)
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      //Here we listen for the MethodChannel call and do stuff
+      MethodChannel(flutterView, CHANNEL).setMethodCallHandler { methodCall, result ->
+        if (methodCall.method == "suCheck") {
+          if (Shell.SU.available()) {
+            result.success(true)
+          }
         }
-      } else {
-        result.notImplemented()
+        var toggle: String = "";
+        when (methodCall.method) {
+          "enableM2" -> toggle = "enable"
+          "disableM2" -> toggle = "disable"
+        }
+        // Ran as blocking asynchronous call to run the enable/disable script
+        val script = async(CommonPool) { Shell.SU.run("sh /sdcard/Download/m2/m2_toggle.sh $toggle") }
+        val await = async (CommonPool) { if (script.await().isSuccessful) result.success(true) 
+        else result.error("UNAVAILABLE", "Script wasn't ran", null) }
       }
+      GeneratedPluginRegistrant.registerWith(this)
     }
-
-    GeneratedPluginRegistrant.registerWith(this)
-  }
-
-  private fun enableM2(root: Boolean): Boolean {
-    if (root){
-      // * First we kill massaging if it's running
-      Shell.SU.run("am force-stop com.google.android.apps.messaging")
-      // * Then we replace the first value in /data/data/com.google.apps.messaging/shared_prefs/PenotypePrefs.xml
-      Shell.SU.run("sed -i '/<boolean name=\"bugle_phenotype__enable_m2\" value=\"false\" \/\>/c\    <boolean name=\"bugle_phenotype__enable_m2\" value=\"true\" \/\>' /data/data/com.google.android.apps.messaging/shared_prefs/PhenotypePrefs.xml")
-      // * And lastly replace the next value
-      Shell.SU.run("sed -i '/<boolean name=\"bugle_phenotype__enable_phenotype_override\" value=\"false\" \/\>/c\    <boolean name=\"bugle_phenotype__enable_phenotype_override\" value=\"true\" \/\>' /data/data/com.google.android.apps.messaging/shared_prefs/PhenotypePrefs.xml")
-      return true
-    } else {
-      return false
-    }
-  }
-
-  private fun disableM2(root: Boolean): Boolean {
-    if (root){
-      // * First we kill massaging if it's running
-      Shell.SU.run("am force-stop com.google.android.apps.messaging")
-      // * Then we replace the first value in /data/data/com.google.apps.messaging/shared_prefs/PenotypePrefs.xml
-      Shell.SU.run("sed -i '/<boolean name=\"bugle_phenotype__enable_m2\" value=\"true\" \/\>/c\    <boolean name=\"bugle_phenotype__enable_m2\" value=\"false\" \/\>' /data/data/com.google.android.apps.messaging/shared_prefs/PhenotypePrefs.xml")
-      // * And lastly replace the next value
-      Shell.SU.run("sed -i '/<boolean name=\"bugle_phenotype__enable_phenotype_override\" value=\"true\" \/\>/c\    <boolean name=\"bugle_phenotype__enable_phenotype_override\" value=\"false\" \/\>' /data/data/com.google.android.apps.messaging/shared_prefs/PhenotypePrefs.xml")
-      return true
-    } else {
-      return false
-    }
-  }
-
-  private fun checkForRoot(enable: Boolean) {
-    object : AsyncTask<Void, Void, Boolean>() {
-      override fun doInBackground(vararg params: Void?): Boolean {
-        return Shell.SU.available()
-      }
-      override fun onPostExecute(available: Boolean) {
-        if (enable) {enableM2(available)}
-        else {disableM2(available)}
-      }
-    }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-  }
 }
